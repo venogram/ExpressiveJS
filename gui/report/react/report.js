27451cee919e1a4da4b73ced1fecbd5393add6ef
Kangseon Cho <kangseon.cho@gmail.com>
2017-04-22 21:12:08 -0700

    working timeline log

--- gui/report/react/report.js
+++ gui/report/react/report.js
@@ -2,12 +2,28 @@ import React, { Component } from 'react';
 
 class Report extends Component {
   render() {
+    //this.props.userReports === timeline array
+    //console.log(this.props.userReports)
+    let report = this.props.userReports.map((element, index) => {
+      // console.log(element[0])
+      return element.map(el => {
+        if (typeof el === "object") {
+          for (let key in el) {
+            // console.log("key", key)
+            // console.log("el: ", el[key])
+            return key + ': ' + el[key]
+          }
+          return el
+        }
+      })
+    })
 
-
+    console.log(report)
 
     return (
       <div className="report">
         <p>i am from report.js</p>
+        <p>{report}</p>
       </div>
     );
   }
--- gui/report/react/route.js
+++ gui/report/react/route.js
@@ -2,18 +2,23 @@ import React, { Component } from 'react';
 import Report from './report';
 
 class Route extends Component {
+  // constructor(props) {
+  //   super(props)
+  // }
   render() {
     //this is an array of userRoutes
     //console.log(this.props.userRoutes)
-    let methodRouteButtons = this.props.userRoutes.map((element, index) => {
-    return <span key={index}><button key={index}> {element} </button><br/></span>
-  });
+
+    //console.log(this.props.displayReport)
+    let methodRouteButtons = this.props.userRoutes.map((element, index, arr) => {
+      return <span key={index}><button key={index} onClick={() => this.props.displayReport(element)}> {element} </button><br/></span>
+    });
 
     return (
       <div className="route">
         <p>i am from route.js</p>
         {methodRouteButtons}
-        <Report watchData={this.props.watchData} userRoutes={this.props.userRoutes} />
+        <Report watchData={this.props.watchData} userRoutes={this.props.userRoutes} userReports={this.props.userReports} />
       </div>
     );
   }
