eff1e5846ac16121e8a27f0a5836c1c541dda44a
Glenn Alexander <glennalexander2014@u.northwestern.edu>
2017-04-24 19:14:19 -0700

    added test files and server listener outline

---
+++ testHtml/test-js/listener-test.js
@@ -0,0 +1,19 @@
+// document.getElementById('myButton').addEventListener('click', () => {
+//   console.log("your click makes me giggle");
+//   const myReq = new XMLHttpRequest();
+//   myReq.open('GET', 'http://localhost:3000/route', true);
+//   myReq.send();
+// })
+
+$('#myButton').on('click', () => {
+  console.log("your click makes me giggle");
+  $('body').load('/route', (data) => {
+    console.log("i done got get");
+  });
+})
+
+$('#closeButton').on('click', () => {
+  $.get('/close', () => {
+    console.log("i done got done");
+  });
+})
