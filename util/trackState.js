82c8ca69882d0ac56be350d022d986ed569caff0
Ryan Fowlkes Smith <ryan.fowlkes@gmail.com>
2017-04-26 10:41:35 -0700

    comments and small changes

--- util/trackState.js
+++ util/trackState.js
@@ -2,9 +2,6 @@
   trackState is middleware that is placed in between each of the developers middleware.
   it takes a snapshot of the current state of the req and res objects and pushes
   the snapshots into res.locals._WD.timeline
-
-  TODO: fix so that it matches the current format of res.locals._WD
-  TODO: write res.locals._WD to watchDog.json
 */
 
 const takeSnapshot = require('./takeSnapshot.js'),
@@ -22,7 +19,6 @@ module.exports = (req, res, next) => {
   wd.timeline.push(snapshot);
   fs.writeFile(path.join(__dirname, './../watchDog.json'), JSON.stringify(wd), (err) => {
     if (err) throw err;
-    console.log('wrote file!!');
   });
   if (next) next();
 }
