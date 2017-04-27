24a44d9994d391a23084bc79d01fa6e012a59294
Ryan Fowlkes Smith <ryan.fowlkes@gmail.com>
2017-04-27 00:53:23 -0700

    can start devServer with node child process

--- cli/testRoutes.js
+++ cli/testRoutes.js
@@ -1,19 +1,14 @@
 const request = require('request');
-const express = require('express');
-const config = require('../watchDog.config.js');
 const path  = require("path");
-const testRoutes = config["testRoutes"]
 
-//SUPERTEST WITHOUT TESTING LIBRARIES 
-// const request = require('supertest');
-// const express = require('express');
-// const app = express();
-request("http://localhost:3000/")
+const config = require('../watchDog.config.js');
+const testRoutes = config.testRoutes;
+const host = config.host;
 
 for(let i = 0; i < testRoutes.length; i++){
    let route = 'http://localhost:3000'+ (testRoutes[i]["route"]);
    console.log(route)
-   request(route); 
+   request(route);
 }
 
 
@@ -28,11 +23,11 @@ for(let i = 0; i < testRoutes.length; i++){
 //   });
 // function testAllRoutes() {
 //   for (let i = 0; i < testRoutes.length; i++) {
-//     createRouteTests(testRoutes[i]["method"], testRoutes[i]["route"], testRoutes[i]["body"]) 
+//     createRouteTests(testRoutes[i]["method"], testRoutes[i]["route"], testRoutes[i]["body"])
 //   }
 // }
 // testAllRoutes();
 
 // function createRouteTests(method, route, body){
-//   request(app).method("'" + route + "'"); 
+//   request(app).method("'" + route + "'");
 // }
