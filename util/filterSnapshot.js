6ab7630130fa569de80ef4e54ab27cd2536904b5
Ryan Fowlkes Smith <ryan.fowlkes@gmail.com>
2017-04-26 20:15:53 -0700

    UNFINISHED - filters snapshots based on config settings

---
+++ util/filterSnapshot.js
@@ -0,0 +1,27 @@
+/*
+  uses the config file to filter out unecessary information from the req and res
+  snapshots
+
+  TODO: make sure this function is being used in initTracking & trackState
+*/
+
+const config = require('./../watchDog.config.js');
+
+function filterSnapshot(snapshot, reX) {
+  if (reX !== 'req' && reX !== 'res') throw new Error('bad input in filterSnapshot');
+  const result = {};
+  const paths = (reX === 'req' ? config.reqWatch : config.resWatch);
+
+  paths.forEach(path => {
+    Object.keys(snapshot).forEach(key => {
+      if (path.indexOf(key) === 0) {
+        const pathArr = path.split('.');
+
+      }
+    })
+  })
+
+
+  })
+
+}
