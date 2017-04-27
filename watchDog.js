0ec200a2294404e5fac8184eff92b8adff69eb5d
Ryan Fowlkes Smith <ryan.fowlkes@gmail.com>
2017-04-27 12:48:19 -0700

    cli runWatchDog starts server as child process and server sends parent process message once its listening

--- watchDog.js
+++ watchDog.js
@@ -1,4 +1,8 @@
 /*
+  watchDog.js wraps express, inserting middleware to track how the server
+  response and client request objects change as they travel through the developer's
+  middleware.
+
   TODO: figure out where and when to reset the JSON file. right now, the set
   function assumes JSON file is configured
   TODO: test ALL the things!
@@ -18,32 +22,29 @@ function set(method, ...args) {
 }
 
 const watchDog = () => {
-  //EVENTUALLY THIS LINE WILL RUN ONCE WHEN WE RUN WATCHDOG FROM THE COMMAND
-  // (I.E. MOVE THIS TO CLI DIRECTORY)
-  jsonController.createJSON();
-
   const watchDogObj = {
-
     get: (...args) => set('GET', ...args),
-
     post: (...args) => set('POST', ...args),
-
     put: (...args) => set('PUT', ...args),
-
     delete: (...args) => set('DELETE', ...args),
 
     listen: (...args) => {
       const server = app.listen(...args);
+
+      //set up server listeners!
       Object.keys(serverListeners).map(event => {
         server.on(event, serverListeners[event]);
       })
+
+      //
+      process.send('listening');
+
       return server;
     }
-
   }
 
-  //assign all properties and methods of the express app to the watchDogObj that aren't
-  //explicitly defined
+  //assign all properties and methods of the express app to the watchDogObj that
+  //aren't explicitly defined
   Object.keys(app).forEach(key => {
     if (!watchDogObj.hasOwnProperty(key)) watchDogObj[key] = app[key];
   });
