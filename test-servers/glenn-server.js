652e778af259860017bb235633211172323b5b2b
Glenn Alexander <glennalexander2014@u.northwestern.edu>
2017-04-28 11:54:19 -0700

    Added comments on stuff to test

--- test-servers/glenn-server.js
+++ test-servers/glenn-server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const WatchDog = require('./../watchDog.js');
 const path = require('path');
-// const app = WatchDog();
-const app = express();
+const app = WatchDog();
+// const app = express();
 
 console.log('hello from glenn server');
 
@@ -20,15 +20,16 @@ function addCookie(req, res, next) {
   return next();
 }
 
-app.use('/route', addCookie);
+// app.use('/route', addCookie);
 
 app.get('/', (req, res) => {
   console.log('getting /');
   res.redirect('/route');
 })
 
-app.get('/route', (req, res) => {
+app.get('/route', addCookie, (req, res) => {
   console.log('getting /route');
+  // console.log('redirect Count:', res.locals._WD.redirectCount);
   res.sendFile(path.resolve(__dirname + '/../testHtml/destination.html'));
 })
 
