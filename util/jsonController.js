d6dbb0b612deb780a03f1e82e83e3bde7593550b
Glenn Alexander <glennalexander2014@u.northwestern.edu>
2017-04-28 11:55:21 -0700

    addRoute now creates method key if not in json

--- util/jsonController.js
+++ util/jsonController.js
@@ -19,17 +19,16 @@ const jsonController = {
   //Creates skeleton JSON file
   createJSON: () => {
     const skeleton = { routes: {} };
-    const methods = ['ALL', 'CHECKOUT', 'COPY', 'DELETE', 'GET', 'HEAD', 'LOCK', 'MERGE', 
-    'MKACTIVITY', 'MKCOL', 'MOVE', 'M-SEARCH', 'NOTIFY', 'OPTIONS', 'PATCH', 'POST',
-    'PURGE', 'PUT', 'REPORT', 'SEARCH', 'SUBSCRIBE', 'TRACE', 'UNLOCK', 'UNSUBSCRIBE'];
-    methods.forEach(method => skeleton.routes[method] = []);
     jsonController.overwrite(skeleton);
   },
 
   //Stores a route and request method that the server is listening for
   addRoute: (method, route) => {
     const parsed = jsonController.getAndParse();
-    if (!parsed.routes[method].includes(route)) {
+    if (!parsed.routes[method]) {
+      parsed.routes[method] = [route];
+      jsonController.overwrite(parsed);
+    } else if (!parsed.routes[method].includes(route)) {
       parsed.routes[method].push(route);
       jsonController.overwrite(parsed);
     }
@@ -38,7 +37,7 @@ const jsonController = {
   //Returns a boolean for whether a given route is stored under a given method in JSON file
   containsRoute: (method, route) => {
     const parsed = jsonController.getAndParse();
-    return parsed.routes[method].includes(route);
+    return parsed.routes[method].includes(route) || parsed.routes.ALL.includes(route);
   },
 
   //Overwrites existing JSON file or creates a new one with a new JSON object
