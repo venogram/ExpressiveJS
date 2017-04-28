cc981d4126c56101b530d78fe8a1c6b5b3099707
Ryan Fowlkes Smith <ryan.fowlkes@gmail.com>
2017-04-28 00:01:06 -0700

    no change

--- test-servers/ryan-server.js
+++ test-servers/ryan-server.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const WatchDog = require('./../watchDog.js');
 const path = require('path');
-const app = WatchDog();
+const app = express();
 
 console.log('hi from ryan');
 
 app.get('/', (req, res, next) => {
   console.log('======GET /=====');
   res.cookie('cookie1', 'hello world');
+  res.redirect('/redirect');
   return next();
 }, (req, res, next) => {
   res.send('response sent!');
 });
+
+app.get('/redirect', (req, res) => {
+  console.log(res);
+  res.send('you were redirected!');
+})
 
 app.get('/route', (req, res) => {
   console.log('gettin dat route route');
