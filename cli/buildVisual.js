a3a3f95665b6f88a6061e002ed024799dcc60082
Glenn Alexander <glennalexander2014@u.northwestern.edu>
2017-04-27 23:39:40 -0700

    added then removed  console.logs

--- cli/buildVisual.js
+++ cli/buildVisual.js
@@ -4,4 +4,5 @@ const exec = require('child_process').exec;
 const path = require('path'); 
 const guiPath = path.join(__dirname, "../gui"); 
 exec(`cd ${guiPath} && webpack`, (err, stdout, stderr) =>{
+
 })
