36e26d9de0bd4f7eefcc5c0875bed791f66c4df3
Glenn Alexander <glennalexander2014@u.northwestern.edu>
2017-04-29 15:05:55 -0700

    the great renaming part II

--- cli/test-routes.js
+++ cli/test-routes.js
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
 
 /*
-  command line script that runs watchdog tests, then bundles the front end, and
+  command line script that runs expressive tests, then bundles the front end, and
   then opens electron to display the html
 */
 
 const Promise = require('bluebird');
 const exec = require('child_process').exec;
 
-const runWatchDog = new Promise((resolve, reject) => {
+const runExpressive = new Promise((resolve, reject) => {
   exec("wd", (err) => {
     if (err) reject(err);
     else resolve();
   });
 });
 
-runWatchDog
+runExpressive
   .then(() => {
     exec("build")
   })
