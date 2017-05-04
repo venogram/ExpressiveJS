32a9ee37532d69622417106770ad95ec49a3a3fb
Kangseon Cho <kangseon.cho@gmail.com>
2017-05-03 22:10:25 -0700

    got snapshots to appear

---
+++ gui/report/react/allSnapshots.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import MethodAndRouteTabs from './methodAndRouteTabs';
+
+class AllSnapshots extends Component {
+  render() {
+
+    //generate report based on userReports (which is our timeline)
+    let snapshot = this.props.userReports.map((element, index, array) => {
+      //access req,res object, and redirect Object (if any)
+      let reqObj = element['req'];
+      let resObj = element['res'];
+
+      if(index === array.length - 1) {
+        return <div key={index}>
+      <div className="snapshot">
+        <p>{this.props.currTab}</p>
+        <p>{element.prevFunc}</p>
+        {/*should have onClick to show component with more information on respective click*/}
+        <p>request state</p>
+        <p>response state</p>
+      </div>
+
+      </div>
+      }
+
+      return <div key={index}>
+      <div className="snapshot">
+        <p>{this.props.currTab}</p>
+        <p>{element.prevFunc}</p>
+        {/*should have onClick to show component with more information on respective click*/}
+        <p>request state</p>
+        <p>response state</p>
+      </div>
+        <img className="chain" src="./../yellowLine.png" />
+      </div>
+    });
+
+    return (
+      <div className='reportColumn' >
+
+        <MethodAndRouteTabs displayReport={this.displayReport} displayReportFromTabs={this.props.displayReportFromTabs}
+          openTabs={this.props.openTabs} currTab={this.props.currTab} closeTab={this.props.closeTab} initTab={this.props.initTab}/>
+
+        <div id="reportSummary" className="flex-item">
+          <div id="toggleView">...</div> {/* going to becoming a react component */}
+          <div id="snapshotChain">
+            {snapshot}
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default AllSnapshots;
--- gui/report/react/methodAndRouteTabs.js
+++ gui/report/react/methodAndRouteTabs.js
@@ -13,7 +13,7 @@ class MethodAndRouteTabs extends Component {
       return <div key={index} className={"flex-tab " + this.initClassName(Object.keys(element)[0], index)}>
         <img className="tabLogo" src="./../public/images/whiteTabLogo@2x.png" />
         <button id={index} className="tabs" onClick={() => { this.props.displayReportFromTabs(Object.keys(element)[0]); this.props.initTab(Object.keys(element)[0]) }}>{Object.keys(element)[0].toLowerCase()}</button>
-        <span className={"tabs hover cancel"} onClick={() => this.props.closeTab(index)}>x</span>
+        <span className={"hover cancel"} onClick={() => this.props.closeTab(index)}>x</span>
       </div>
     });
 
