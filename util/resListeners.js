8eeec64766a8094e7c2753f5f2ceeded9bb11bd3
Glenn Alexander <glennalexander2014@u.northwestern.edu>
2017-05-06 12:35:42 -0700

    implemented linked list of reports in json object

--- util/resListeners.js
+++ util/resListeners.js
@@ -12,46 +12,55 @@ const Report = Classes.Report;
 const resListeners = {
   //passed as callback into onFinish
   finish: (err, res) => {
-    const now = Date.now();
     let xpr = res.locals._XPR;
     let validRequest = true;
-    // no xpr means this is a request to an invalid route i.e. 404 error
+    let isRedirect = false;
+    // no xpr means this is a request to an invalid route (i.e. 404 error)
     if (!xpr) {
-      const parsed = jsonController.getAndParse();
-      // if there is a currentRoute then this is a redirect
-      if (parsed.currentRoute) {
-        jsonController.updateCurrentReport(parsed, (report) => {
-          report.redirect = new Report(res.req, res, "initial state", now);
-        })
-        parsed.currentRoute.push('redirect');
-      } else {
-        let methodRoute = res.req.method + " " + res.req.originalUrl;
-        parsed.currentRoute = [methodRoute];
-        parsed[methodRoute] = new Report(res.req, res, "inital state", now);
-      }
+      xpr = jsonController.getAndParse();
       validRequest = false;
-      xpr = parsed;
+    }
+    // if currentRoute has been defined then request is a redirect
+    // otherwise it is a client request
+    if (xpr.currentRoute) {
+      jsonController.updateCurrentReport(xpr, (report) => {
+        report.next = new Report(res.req, res, report.nextFuncName, null, true);
+      })
+      isRedirect = true;
+    } else {
+      let methodRoute = res.req.method + ' ' + res.req.originalUrl;
+      if (xpr[methodRoute]) {
+        let newMethodRoute = methodRoute;
+        for (let i = 2; xpr[newMethodRoute]; i += 1) {
+          newMethodRoute = methodRoute + ' ' + i;
+        }
+        methodRoute = newMethodRoute;
+      }
+      xpr.currentRoute = methodRoute;
+      xpr[methodRoute] = new Report(req, res, 'initial state', null);
     }
 
-    let reportDuration;
-    jsonController.updateCurrentReport(xpr, (report) => {
-      if (validRequest) report.timeline.push(new Snapshot(res.req, res, report.midware[report.midware.length - 1], now));
-      report.end = now;
-      reportDuration = report.duration = report.end - report.start;
-      report.error = err;
-      report.statusCode = res.statusCode;
-      report.statusMessage = res.statusMessage;
-    })
 
-    const finishDuration = Date.now() - now;
-    //increments totalDuration in initial report with duration of current report
-    xpr[xpr.currentRoute[0]].hasOwnProperty('totalDuration') ?
-      xpr[xpr.currentRoute[0]].totalDuration += reportDuration - finishDuration :
-      xpr[xpr.currentRoute[0]].totalDuration = reportDuration - finishDuration;
 
-    if (!xpr[xpr.currentRoute[0]].isRedirect) xpr.completedReqs += 1;
+    // let reportDuration;
+    // jsonController.updateCurrentReport(xpr, (report) => {
+    //   if (validRequest) report.timeline.push(new Snapshot(res.req, res, report.midware[report.midware.length - 1], now));
+    //   report.end = now;
+    //   reportDuration = report.duration = report.end - report.start;
+    //   report.error = err;
+    //   report.statusCode = res.statusCode;
+    //   report.statusMessage = res.statusMessage;
+    // })
+
+    // const finishDuration = Date.now() - now;
+    // //increments totalDuration in initial report with duration of current report
+    // xpr[xpr.currentRoute[0]].hasOwnProperty('totalDuration') ?
+    //   xpr[xpr.currentRoute[0]].totalDuration += reportDuration - finishDuration :
+    //   xpr[xpr.currentRoute[0]].totalDuration = reportDuration - finishDuration;
+
+    if (!isRedirect) xpr.completedReqs += 1;
     jsonController.overwrite(xpr);
-    if (!xpr[xpr.currentRoute[0]].isRedirect || !validRequest) process.send('next');
+    if (!isRedirect || !validRequest) process.send('next');
   }
 
 }
