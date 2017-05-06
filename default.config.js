d15a896bcc19d40ec1c5a9efc60be5ec9f51357b
Ryan Fowlkes Smith <ryan.fowlkes@gmail.com>
2017-05-06 15:57:45 -0700

    no more overwrite test

--- default.config.js
+++ default.config.js
@@ -7,19 +7,17 @@ const config = {
   entry: './test-servers/ryan-server.js', //relative path to server file to test
   host: 'http://localhost:3000', // port the dev server listens to
   testRoutes: [
-    {method: "GET", uri:"/"},
-    {method: "GET", uri:"/redirect"},
-    {method: "POST", uri:"/"}
+    {method: "GET", uri:"/", jar: true},
+    {method: "GET", uri:"/redirect", jar: true},
+    {method: "POST", uri:"/", jar: true}
   ], // specify routes to test. objects must be formatted according to the request module options
   useDefaults: true, // if false, default config will not augment provided config
-
+  silentServer: false, // if false, xpr command will log server's log statements
 
   // YET TO IMPLEMENT CONFIG SETTINGS
   watch: ['headers', 'response body', 'request data'], // flag certain things in req/res objects to follow
-  abandonRequest: 5, // time (per request) to wait before abandoning the request
-  silentServer: true, // if false, xpr command will log server's log statements
+  abandonRequest: 3, // time (per request) to wait before abandoning the request
   wipeCookies: false, // if true, cookies are cleared in between each request
-  overwriteTest: true, // if false, a second test will not override first test
 }
 
 module.exports = config;
