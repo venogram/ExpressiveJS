5a39f1ff049d222dbe9f444a0ca5eb3e51334c8c
Ryan Fowlkes Smith <ryan.fowlkes@gmail.com>
2017-05-08 16:50:33 -0700

    xpr now runs three commands sequentially and xpr-display exits without closing electron

--- cli/buildVisual.js
+++ cli/buildVisual.js
@@ -1,10 +1,20 @@
 #!/usr/bin/env node
 
 //Runs Webpack to Update Visualization on Changes
-
+const ProgressBar = require('progress');
 const exec = require('child_process').exec;
-const path = require('path'); 
-const guiPath = path.join(__dirname, "../gui"); 
-exec(`cd ${guiPath} && webpack`, (err, stdout, stderr) =>{
+const path = require('path');
+const guiPath = path.join(__dirname, "../gui");
 
-})
+// const bar = new ProgressBar(' webpack bundling [:bar]', {
+//   complete: '=',
+//   incomplete: ' ',
+//   total: 2
+// });
+//
+// bar.tick();
+
+console.log('Bundling graphic user interface...');
+exec(`cd ${guiPath} && webpack`, () => {
+  // bar.tick();
+});
