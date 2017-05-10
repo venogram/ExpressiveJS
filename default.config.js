7bebc887728d9aa8950aa29c64e22cce48f97baf
Ryan Fowlkes Smith <ryan.fowlkes@gmail.com>
2017-05-09 20:25:12 -0700

    got rid of watch option

--- default.config.js
+++ default.config.js
@@ -4,20 +4,22 @@
 */
 
 const config = {
-  entry: './test-servers/glenn-server.js', //relative path to server file to test
+  entry: './test-servers/ryan-server.js', //relative path to server file to test
   host: 'http://localhost:3000', // port the dev server listens to
   testRoutes: [
-    {method: "GET", uri:"/", jar: true}
+    {method: "GET", uri:"/"},
+    {method: "GET", uri:"/redirect"},
+    {method: "GET", uri:"/route1"}
+    // {method: "POST", uri:"/", body:"i am a request body"},
+    // {method: "GET", uri:"/route1"},
+    // {method: "GET", uri:"/route2"},
+    // {method: "GET", uri:"/route3"}
   ], // specify routes to test. objects must be formatted according to the request module options
-  useDefaults: true, // if false, default config will not augment provided config
   silentServer: false, // if false, xpr command will log server's log statements
   wipeCookies: false, // if true, cookies are cleared in between each request
-
+  outputDir: '/',
   //Should test with database calls
   abandonRequest: 3, // time (per request) to wait before abandoning the request
-
-  // YET TO IMPLEMENT CONFIG SETTINGS
-  watch: ['headers', 'response body', 'request data'], // flag certain things in req/res objects to follow
 }
 
 module.exports = config;
