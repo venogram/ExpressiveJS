fedd31c12954ca0687d65d243f0cf42391a9d711
Glenn Alexander <glennalexander2014@u.northwestern.edu>
2017-05-10 14:15:22 -0700

    expressive now modifies instance of express app, rather than creating expressiveObj and assigning it express app methods

--- expressive.js
+++ expressive.js
@@ -6,125 +6,111 @@
 */
 
 const express = require('express');
-const app = express();
 const getAppMethodArgs = require('./util/getAppMethodArgs.js');
 const jsonController = require('./util/jsonController.js');
 const serverListeners = require('./util/serverListeners.js');
-
-
-// //wrapper for app.METHOD
-// //wraps developer midware with expressive midware
-// function insertExpressiveMidware(...args) {
-//   const expressiveMidware = getAppMethodArgs(args);
-//   return myApp[method](...expressiveMidware);
-// }
-
-// //stores route and method in json object for creation of default config file
-// //then wraps app.METHOD
-// function set(...args) {
-//   const route = args[0];
-//   jsonController.addRoute(method, route);
-//   return insertExpressiveMidware(myApp, method, ...args);
-// }
 
 const requestMethods = ['all', 'checkout', 'copy', 'delete', 'get', 'head', 'lock', 'merge',
   'mkactivity', 'mkcol', 'move', 'm-search', 'notify', 'options', 'patch', 'post',
   'purge', 'put', 'report', 'search', 'subscribe', 'trace', 'unlock', 'unsubscribe'];
 
-const expressive = () => {
-  const expressiveObj = {
 
-    listen: (...args) => {
-      const server = app.listen(...args);
-      //set up server listeners!
-      Object.keys(serverListeners).forEach(event => {
-        server.on(event, serverListeners[event]);
-      });
-      //sends message to parent process that it may start firing requests!
-      if (process.send) process.send('listening');
-      return server;
-    },
+const originalRouter = express.Router;
+express.Router = (...routerArgs) => {
+  const router = originalRouter.call(express, ...routerArgs);
 
-    use: (...args) => app.use(...getAppMethodArgs(args)),
-
-    route: (path) => {
-      const returnedRoute = app.route(path);
-
-      requestMethods.forEach(method => {
-        const originalMethod = returnedRoute[method];
-        returnedRoute[method] = (...args) => originalMethod.call(returnedRoute, ...getAppMethodArgs(args));
-      });
-
-      const originalUse = returnedRoute.use;
-      returnedRoute.use = (...args) => originalUse.call(returnedRoute, ...getAppMethodArgs(args));
-
-      return returnedRoute;
-    },
-
-    param: (...args) => app.param(...getAppMethodArgs(args)),
-
-    // These do not alter the request and response so there is no need to track them
-    // disable: () => {},
-    // disabled: () => {},
-    // enable: () => {},
-    // enabled: () => {},
-    // engine: () => {},
-    // path: () => {},
-    // render: () => {},
-    // set: () => {},
-  }
-
-  //assigns app.METHOD for all request methods
   requestMethods.forEach(method => {
-    expressiveObj[method] = (...args) => app[method](...getAppMethodArgs(args));
-  });
+    const originalMethod = router[method];
+    router[method] = (...methodArgs) => originalMethod.call(router, ...getAppMethodArgs(methodArgs));
+  })
 
+  const originalUse = router.use;
+  router.use = (...useArgs) => originalUse.call(router, ...getAppMethodArgs(useArgs));
 
-  expressive.Router = (...args) => {
-    const router = express.Router(...args);
+  const originalParam = router.param;
+  router.param = (...paramArgs) => originalParam.call(router, ...getAppMethodArgs(paramArgs));
+
+  const originalRoute = router.route;
+  router.route = (path) => {
+    const route = originalRoute.call(router, path);
 
     requestMethods.forEach(method => {
-      const originalMethod = router[method];
-      router[method] = (...methodArgs) => originalMethod.call(router, ...getAppMethodArgs(methodArgs));
+      const originalMethod = route[method];
+      route[method] = (...methodArgs) => originalMethod.call(route, ...getAppMethodArgs(methodArgs));
     })
-    
-    const originalUse = router.use;
-    router.use = (...useArgs) => originalUse.call(router, ...getAppMethodArgs(methodArgs));
 
-    const originalParam = router.param;
-    router.param = (...paramArgs) => originalParam.call(router, ...getAppMethodArgs(paramArgs));
+    const originalUse = route.use;
+    route.use = (...useArgs) => originalUse.call(route, ...getAppMethodArgs(useArgs));
 
-    const originalRoute = router.route;
-    router.route = (path) => {
-      const returnedRoute = originalRoute.call(router ,path);
-
-      requestMethods.forEach(routeMethod => {
-        const originalMethod = returnedRoute[routeMethod];
-        returnedRoute[routeMethod] = (...routeArgs) => originalMethod.call(returnedRoute, ...getAppMethodArgs(routeArgs));
-      })
-
-      const originalRouteUse = returnedRoute.use;
-      returnedRoute.use = (...routeUseArgs) => originalRouteUse.call(returnedRoute, ...getAppMethodArgs(routeUseArgs));
-
-      return returnedRoute;
-    }
-
-    return router;
+    return route;
   }
 
-
-
-  //FALLBACKS: assign all undefined properties and methods of express and express app 
-  //to expressive and expressiveObj respectively
-  Object.keys(app).forEach(key => {
-    if (!expressiveObj.hasOwnProperty(key)) expressiveObj[key] = app[key];
-  });
-  Object.keys(express).forEach(key => {
-    if (!expressive.hasOwnProperty(key)) expressive[key] = express[key];
-  });
-
-  return expressiveObj;
+  return router;
 }
+
+
+const expressive = () => {
+  const app = express();
+
+  const originalListen = app.listen;
+  app.listen = (...listenArgs) => {
+    const server = originalListen.call(app, ...listenArgs);
+    //set up server listeners!
+    Object.keys(serverListeners).forEach(event => {
+      server.on(event, serverListeners[event]);
+    });
+    //sends message to parent process that it may start firing requests!
+    if (process.send) process.send('listening');
+    return server;
+  };
+
+  const originalUse = app.use;
+  app.use = (...useArgs) => originalUse.call(app, ...getAppMethodArgs(useArgs));
+
+  const originalRoute = app.route;
+  app.route = (path) => {
+    const route = originalRoute.call(app, path);
+
+    requestMethods.forEach(method => {
+      const originalMethod = route[method];
+      route[method] = (...methodArgs) => originalMethod.call(route, ...getAppMethodArgs(methodArgs));
+    });
+
+    const originalUse = route.use;
+    route.use = (...useArgs) => originalUse.call(route, ...getAppMethodArgs(useArgs));
+
+    return route;
+  }
+
+  const originalParam = app.param;
+  app.param = (...args) => originalParam.call(app, ...getAppMethodArgs(args))
+
+  requestMethods.forEach(method => {
+    const originalMethod = app[method];
+    app[method] = (...args) => originalMethod.call(app, ...getAppMethodArgs(args));
+  });
+  
+// These do not alter the request and response so there is no need to track them
+// app.disable: () => {};
+// app.disabled: () => {};
+// app.enable: () => {};
+// app.enabled: () => {};
+// app.engine: () => {};
+// app.path: () => {};
+// app.render: () => {};
+// app.set: () => {};
+
+  return app;
+}
+
+
+//Assign all properties and methods of express to expressive
+Object.keys(express).forEach(key => {
+  if (typeof express[key] === 'function') {
+    const originalMethod = express[key];
+    expressive[key] = (...methodArgs) => originalMethod.call(expressive, ...methodArgs);
+  } else expressive[key] = express[key];
+});
 
 
 
